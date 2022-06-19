66192914c2dbb3a9a5ad6c59d928b9355e9d8587
yoshiki <tanayoshi78907890@icloud.com>
2022-06-19 21:40:34 +0900

    comment out

--- HackerRankFileTitle.py
+++ HackerRankFileTitle.py
@@ -1,18 +1,21 @@
 import os
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver #モジュールのインポート
+from selenium import webdriver
 
-browser = webdriver.Chrome() #ブラウザの立ち上がる
-browser.get('https://www.hackerrank.com/domains/python') 
+#ブラウザの立ち上がる
+browser = webdriver.Chrome() 
+browser.get('https://www.hackerrank.com/domains/python') #この部分でエラー発生中
 
 html = browser.find_element_by_class_name('challengecard-title')
 print(html)
 
 '''
-# title タグの文字列を取得する
-title_array = []
+# beautifulsoupを使用したデータ取得（スクロールが発生する場合は使用できない）
 title_texts = soup.find_all(class_='challengecard-title')
+
+# データ整形（ファイル名が見やすいように）
+title_array = []
 for title in title_texts:
     title = title.text
     title = title.split('Easy')[0]
@@ -24,6 +27,7 @@ print(title_array)
 '''
 
 '''
+# ファイル作成
 title = 'a' 
 path = '../HackerRank/' + title + '.py'
 f = open(path, 'w')
